f2ad5c1c089cfb13272ec3da8aeda3e3763780ed
Pierre-Louis242 <plpicard@hotmail.fr>
2019-07-05 16:12:51 +0200

    Challenge done

---
+++ GCD_Finder/main.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+
+    let result = document.getElementById('titleForResult');
+    result.style.display = "none";
+    
+    function gcdFinder(x, y, e) {
+        event.preventDefault(e)
+
+        x = document.getElementById('firstNumber').value;
+        y = document.getElementById('secNumber').value;
+
+        if ((isNaN(x)) || (isNaN(y))) {
+            window.alert("Not a number!!!!! You need to put integers only!!");
+            return false
+        }
+    
+        while(y) {
+            var t = y;
+            y = x % y;
+            x = t;
+        }
+
+        console.log(x); 
+
+        let resultOfGcd = document.getElementById('resultOfGcd'); 
+        result.style.display = "flex";
+        resultOfGcd.innerText = " " + x;
+    }
+
+
+    let form = document.getElementById('formToSent');
+    form.addEventListener('submit', gcdFinder);
+    let button = document.getElementById('submitButtonCss');
+    button.addEventListener('click', gcdFinder);
+
+
+
+}
